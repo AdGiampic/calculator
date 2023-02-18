69f571bf1c712bbc138e294120ece4081bba157b
Unknown-Drifter <adgiampic@gmail.com>
2023-02-18 10:20:37 +0100

    Created functions that operate two numbers and a main function that takes calls the correct function according to the operator

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 document.getElementById('inputCalc').focus();
+
+function add (a,b) {
+    return a + b;
+}
+
+function subtract (a,b) {
+    return a - b;
+}
+
+function multiply (a,b) {
+    return a * b;
+}
+
+function divide (a,b) {
+    return a / b;
+}
+
+function writeNmbr (id) {
+    document.getElementById('inputCalc').innerHTML = id;
+}
+
+function operate (a,b,op) {
+    switch (op) {
+        case '+' :
+            add(a,b);
+            break;
+        case '-' :
+            subtract(a,b);
+            break;
+        case 'x' :
+            multiply (a,b);
+            break;
+        case '/' :
+            divide (a,b);
+            break;
+    }
+}
