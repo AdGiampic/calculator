d9bd7bf6337e19531c0d2dafda46821c031b314e
Unknown-Drifter <adgiampic@gmail.com>
2023-03-04 18:53:43 +0100

    Corrected a bug where a multiplication wasn't triggering while using the keyboard

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ let disp = document.getElementById('displayN');
 function writeNmbr (id) {
     let content = elem.innerHTML;
     content += id; // I keep append the last number into the screen
-    let commas = content.split('.').length-1
+    let commas = content.split('.').length-1;
     if (commas > 1) {  // preventing multiple commas into the screen
         elem.innerHTML = content.substring(0, content.length-1);
         return
@@ -53,6 +53,7 @@ function calc (op) {
     let dispCont = disp.innerHTML;
     if (ops.test(dispCont) == true) { // if another calculation is present I'll do it right away (pair calculation)
         operate();
+        return
     }
     let firstN = elem.innerHTML; //gets the first number
     writeNmbr(op);
@@ -65,6 +66,9 @@ function calc (op) {
 function operate () {
     let content = disp.innerHTML;
     op = content.slice(-1);
+    if (op == '*') {
+        op = 'x'; // convert * key to x in order to trigger multiply when needed
+    }
     a = content.substring(0, content.length-1); // removes last character (operator)
     b = elem.innerHTML;
     if (ops.test(b) == true) { // if an operation is already present I remove it to prevent a Nan return
@@ -86,19 +90,23 @@ function operate () {
         case '%' :
             elem.innerHTML = modulo(+a,+b);
     }
+    disp.innerHTML = '';
 }
 
 // keyboard support
 document.addEventListener('keydown', (event) => {
     let name = event.key;
     // do an array with accepted keynames
-    let acceptedInputs = ['1','2','3','4','5','6','7','8','9','0','/','*','+','-','%','=','Enter','Backspace']
+    let acceptedInputs = ['1','2','3','4','5','6','7','8','9','0','/','*','+','-','%','=','Enter','Backspace','.']
     if (acceptedInputs.includes(name) == true) {
         if (name !== 'Enter' && name !== 'Backspace') { // should exclude some key names
             elem.innerHTML += name;
         }
         let elemCont = elem.innerHTML;
         if (ops.test(elemCont) == true) { // if I press one of the operators I call the calc function
+            if (name == '*') {
+                name = 'x';  // convert * key to x in order to trigger multiply when needed
+            }
             calc(name);
             let dispCont = disp.innerHTML;
             disp.innerHTML = dispCont.substring(0, dispCont.length-1); // prevents to add operation twice but it's causing issues
